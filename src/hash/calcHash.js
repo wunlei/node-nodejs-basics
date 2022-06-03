3e8cd53f5ab588b4a7f854ad9b54ac665deed018
wunlei <confusioncosmos@gmail.com>
2022-06-03 16:12:12 +0300

    feat: implement src/hash calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,33 @@
+import crypto from "crypto";
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+import { checkIsFileExist } from "../utils/checkIsPathExist.js";
+
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.resolve(dirname, "files/fileToCalculateHashFor.txt");
+
+  const isFileExist = await checkIsFileExist(filePath);
+  try {
+    if (!isFileExist) {
+      throw new Error("FS operation failed");
+    }
+  } catch (err) {
+    console.error(err.message);
+    return;
+  }
+
+  try {
+    const hashSum = crypto.createHash("sha256");
+    const content = await fs.readFile(filePath);
+    hashSum.update(content);
+    const hex = hashSum.digest("hex");
+    return hex;
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+console.log(await calculateHash());
