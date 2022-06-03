ad38b1144c440f96eb78b00cde96ce283b68bd5f
wunlei <confusioncosmos@gmail.com>
2022-06-03 20:50:45 +0300

    feat: implement src/streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,26 @@
+import fs from "fs";
+import path from "path";
+import url from "url";
+import { checkIsFileExist } from "../utils/checkIsPathExist.js";
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(dirname, "files/fileToRead.txt");
+  const isFileExist = await checkIsFileExist(filePath);
+  try {
+    if (!isFileExist) {
+      throw new Error("FS operation failed: file doesn't exist");
+    }
+  } catch (err) {
+    console.error(err.message);
+    return;
+  }
+
+  const readStream = fs.createReadStream(filePath);
+  readStream.setEncoding("utf8");
+  readStream.pipe(process.stdout).on("error", (err) => {
+    console.error(err);
+  });
 };
+
+read();
