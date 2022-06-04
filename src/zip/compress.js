127131d5414c1222afecfd6876a8f57bc0edac64
wunlei <confusioncosmos@gmail.com>
2022-06-04 17:01:08 +0300

    feat: implement src/zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,37 @@
+import fs from "fs";
+import path from "path";
+import url from "url";
+import zlib from "zlib";
+import { checkIsFileExist } from "../utils/checkIsPathExist.js";
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const compress = async () => {
-    // Write your code here 
+  const filePath = path.resolve(dirname, "files/fileToCompress.txt");
+  const gzPath = path.resolve(dirname, "files/archive.gz");
+
+  const isFileExist = await checkIsFileExist(filePath);
+  const isGzExist = await checkIsFileExist(gzPath);
+
+  try {
+    if (!isFileExist) {
+      throw new Error("FS operation failed: file doesn't exist");
+    } else if (isGzExist) {
+      throw new Error("FS operation failed: file already exists");
+    }
+  } catch (err) {
+    console.error(err.message);
+    return;
+  }
+
+  const readable = fs.createReadStream(filePath);
+  const writable = fs.createWriteStream(gzPath);
+  const gZip = zlib.createGzip();
+  readable
+    .pipe(gZip)
+    .pipe(writable)
+    .on("error", (err) => {
+      console.error(err);
+    });
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,36 @@
+import fs from "fs";
+import path from "path";
+import url from "url";
+import zlib from "zlib";
+import { checkIsFileExist } from "../utils/checkIsPathExist.js";
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const decompress = async () => {
-    // Write your code here 
+  const filePath = path.resolve(dirname, "files/fileToCompress.txt");
+  const gzPath = path.resolve(dirname, "files/archive.gz");
+  const isFileExist = await checkIsFileExist(filePath);
+  const isGzExist = await checkIsFileExist(gzPath);
+
+  try {
+    if (isFileExist) {
+      throw new Error("FS operation failed: file already exists");
+    } else if (!isGzExist) {
+      throw new Error("FS operation failed: file doesn't exist");
+    }
+  } catch (err) {
+    console.error(err.message);
+    return;
+  }
+
+  const readable = fs.createReadStream(gzPath);
+  const writable = fs.createWriteStream(filePath);
+  const gZip = zlib.createGunzip();
+  readable
+    .pipe(gZip)
+    .pipe(writable)
+    .on("error", (err) => {
+      console.error(err);
+    });
 };
+
+decompress();
