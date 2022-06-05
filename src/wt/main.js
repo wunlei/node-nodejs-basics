dcf531a73b6f649ee64c9a7efb41cdeaf5c5e88c
wunlei <confusioncosmos@gmail.com>
2022-06-05 10:15:58 +0300

    feat: implement src/wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,33 @@
+import { Worker } from "worker_threads";
+import path from "path";
+import url from "url";
+import os from "os";
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
 export const performCalculations = async () => {
-    // Write your code here
+  const filePath = path.resolve(dirname, "worker.js");
+  const cpuCount = os.cpus().length;
+
+  const promiseArray = [];
+
+  for (let i = 0; i < cpuCount; i += 1) {
+    const promise = new Promise((resolve, reject) => {
+      const worker = new Worker(filePath, {
+        workerData: 10 + i,
+      });
+      worker.on("message", (message) => {
+        resolve({ status: "resolved", data: message });
+      });
+      worker.on("error", (error) => {
+        reject({ status: "rejected", data: null });
+      });
+    });
+
+    promiseArray.push(promise);
+  }
+  const result = await Promise.all(promiseArray);
+
+  return result;
 };
+
+console.log(await performCalculations());
