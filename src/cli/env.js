4c7419e9b56e19e4945c2211baa873f209584a5e
wunlei <confusioncosmos@gmail.com>
2022-06-05 21:41:54 +0300

    refactor: add cli/env run instructions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
+//  to run in bash:
+//  RSS_name1=value1 RSS_name2=value2 node src/cli/env.js
+
+// run in powershell:
+// $env:RSS_name1='value1'; $env:RSS_name2='value2'; node src/cli/env.js
+
+// run in cmd:
+// set RSS_name1=value1& set RSS_name2=value2& node src/cli/env.js
+
 export const parseEnv = () => {
   const env = process.env;
   const result = [];
